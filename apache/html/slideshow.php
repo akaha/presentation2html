c120d4beef4b55b220ff24f09d229273c1c8fdfd
Ann-Kathrin Hartmann <annkathrin.hartmann91@gmail.com>
2016-06-16 11:09:36 +0200

    Rename uploaded file

--- apache/html/slideshow.php
+++ apache/html/slideshow.php
@@ -1,61 +1,61 @@
 <!DOCTYPE html>
 <html>
 <head>
-	<title>Converted presentation</title>
-	<style type="text/css">
-		body {
-		    background-repeat: no-repeat;
-			background-size: cover;
-		}
-	</style>
+    <title>Converted presentation</title>
+    <style type="text/css">
+        body {
+            background-repeat: no-repeat;
+            background-size: cover;
+        }
+    </style>
 </head>
 <body onload="setBackground()">
 <script>
-	function setBackground () {
-		var slides = [],
-			pos = 0,
-			dir;
+    function setBackground () {
+        var slides = [],
+            pos = 0,
+            dir;
 
-		<?php
-			if ($_GET["dir"]) {
-				$dir = "output/" . urldecode($_GET["dir"]);
-				if (file_exists($dir)) {
-					$files = scandir($dir);
+        <?php
+            if ($_GET["dir"]) {
+                $dir = "output/" . urldecode($_GET["dir"]);
+                if (file_exists($dir)) {
+                    $files = scandir($dir);
 
-					for($x = 0; $x < count($files); $x++) {
-				    	echo "slides.push('" . $files[$x] . "');";
-					}
-					echo "dir = '" . $dir . "';";
-				}
-			}
-		?>
+                    for($x = 0; $x < count($files); $x++) {
+                        echo "slides.push('" . $files[$x] . "');";
+                    }
+                    echo "dir = '" . $dir . "';";
+                }
+            }
+        ?>
 
-		if (!dir) {
-			return;
-		}
+        if (!dir) {
+            return;
+        }
 
-		slides = slides.filter(function (file) {
-			return file !== "." && file !== ".." && file.split(".").reverse()[0] === "svg";
-		});
+        slides = slides.filter(function (file) {
+            return file !== "." && file !== ".." && file.split(".").reverse()[0] === "svg";
+        });
 
-		var body = document.getElementsByTagName("body")[0];
-		body.style.backgroundImage = "url(" + dir + "/" + slides[pos] + ")";
+        var body = document.getElementsByTagName("body")[0];
+        body.style.backgroundImage = "url(" + dir + "/" + slides[pos] + ")";
 
-		document.addEventListener("keydown", function (event) {
-			switch (event.keyCode) {
-				case 37: // left
-				case 38: // up
-					pos = (pos - 1 >= 0) ? pos - 1 : 0;
-					break;
-				case 39: // right
-				case 40:
-					pos = (pos + 1 < slides.length) ? pos + 1 : slides.length - 1;
-					break;
-				default: return;
-			}
-			body.style.backgroundImage = "url(" + dir + "/" + slides[pos] + ")";
-		});
-	}
-	</script>
+        document.addEventListener("keydown", function (event) {
+            switch (event.keyCode) {
+                case 37: // left
+                case 38: // up
+                    pos = (pos - 1 >= 0) ? pos - 1 : 0;
+                    break;
+                case 39: // right
+                case 40:
+                    pos = (pos + 1 < slides.length) ? pos + 1 : slides.length - 1;
+                    break;
+                default: return;
+            }
+            body.style.backgroundImage = "url(" + dir + "/" + slides[pos] + ")";
+        });
+    }
+    </script>
 </body>
 </html>
--- apache/html/upload.php
+++ apache/html/upload.php
@@ -1,8 +1,9 @@
 <?php
 $target_dir = "/input/";
-$target_file = $target_dir . basename($_FILES["pdf"]["name"]);
+$uid = uniqid("presentation_");
+$target_file = $target_dir . $uid . ".pdf";
 $uploadOk = 1;
-$imageFileType = pathinfo($target_file,PATHINFO_EXTENSION);
+$fileType = pathinfo(basename($_FILES["pdf"]["name"]),PATHINFO_EXTENSION);
 
 // Check if file already exists
 if (file_exists($target_file)) {
@@ -11,8 +12,8 @@ if (file_exists($target_file)) {
 }
 
 // Allow certain file formats
-if($imageFileType != "pdf" ) {
-    echo "Sorry, only pdf files are allowed.". $imageFileType;
+if($fileType != "pdf" ) {
+    echo "Sorry, only pdf files are allowed.". $fileType;
     $uploadOk = 0;
 }
 // Check if $uploadOk is set to 0 by an error
@@ -21,8 +22,7 @@ if ($uploadOk == 0) {
 // If everything is ok, try to upload file
 } else {
     if (move_uploaded_file($_FILES["pdf"]["tmp_name"], $target_file)) {
-        $dir = urlencode(explode(".", $_FILES["pdf"]["name"])[0]);
-        echo "The file ". basename( $_FILES["pdf"]["name"]). " has been uploaded. See <a href='slideshow.php?dir=" . htmlentities($dir) . "'>results</a>";
+        echo "The file ". basename( $_FILES["pdf"]["name"]). " has been uploaded. See <a href='slideshow.php?dir=" . $uid . "'>results</a>";
     } else {
         echo "Sorry, there was an error uploading your file " . $_FILES["pdf"]["tmp_name"] . " to " . $target_file;
     }
